79afdde5cc8e85edf0cc301886717f0375dda895
salam <es_belhadj@esi.dz>
2022-02-24 13:45:07 +0100

    use React basic styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => (
-  <nav className="navbar">
-    <h1>Math Magicians</h1>
-    <div className="links">
-      <NavLink to="/" className="linklist"> Home </NavLink>
-      <NavLink to="/Calculator" className="linklist"> Calculator </NavLink>
-      <NavLink to="/Quote" className="linklist"> Quote </NavLink>
-    </div>
-  </nav>
-);
+const Header = () => {
+  const links = {
+    marginLeft: 'auto',
+    marginRight: '50px',
+  };
+  return (
+    <nav className="navbar">
+      <h1>Math Magicians</h1>
+      <div style={links}>
+        <NavLink to="/" className="linklist"> Home </NavLink>
+        <NavLink to="/Calculator" className="linklist"> Calculator </NavLink>
+        <NavLink to="/Quote" className="linklist"> Quote </NavLink>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
